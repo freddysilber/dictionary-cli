f6332fed7ff2988a01d329f1261b5d2239ad941f
Freddy Silber <freddy.silber@cloudcoach.com>
2019-07-12 22:51:52 -0600

    setting up dictionary project and initial set-up

---
+++ lib/dictionary.rb
@@ -0,0 +1,6 @@
+require "dictionary/version"
+
+module Dictionary
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/dictionary_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Dictionary do
+  it "has a version number" do
+    expect(Dictionary::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
