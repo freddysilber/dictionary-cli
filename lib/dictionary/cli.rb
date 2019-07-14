e2cb7815c9cf6aa5c5f9f40fd814ee641ea27589
Freddy Silber <freddy.silber@cloudcoach.com>
2019-07-14 15:23:34 -0600

    adding logic

--- lib/dictionary/cli.rb
+++ lib/dictionary/cli.rb
@@ -10,22 +10,42 @@
 class Dictionary::CLI
     def call  
         show_word_the_day
+        get_user_input
         search_for_word
+        # display_word
+        # ask if user wants to search again
     end
 
     def show_word_the_day
+        puts "Welcome to Merriam Webster online Dictionary!"
         puts "The word of the day is:"
         @word = Dictionary::Word.today
         puts "--- #{@word.word_name}"
         puts "------ #{@word.part_of_speech}"
-        puts "------ #{@word.definition}"
+        puts "------ Definition A: #{@word.definition}"
     end
     
-    def search_for_word
+    def get_user_input
         puts "-- Search for a word..."
-        input = gets.upcase.strip
-        puts "-- The word you searched #{input}. Is that correct? (Y/N)"
-        confirmation = gets.upcase.strip
-        puts "-- " + confirmation
+        input = gets.downcase.strip
+        puts "-- The word you searched is #{input}. Is that correct? (Y/N)"
+        confirmation = gets.downcase.strip
+        if confirmation == "N" || confirmation == "n" || confirmation == "no" || confirmation == "NO"
+            get_user_input
+        else
+            @word_to_search = input
+        end
+    end
+
+    def search_for_word
+        puts "-- Searching for word: #{@word_to_search}..."
+        @word_searched = Dictionary::Word.search_for_word(@word_to_search)
+        #word_searched.word_name
+        #word_searched.part_of_speech etc..
+        display_word
+    end
+
+    def display_word
+        puts "--- #{@word_searched.word_name}"
     end
 end
--- lib/dictionary/word.rb
+++ lib/dictionary/word.rb
@@ -5,8 +5,16 @@ class Dictionary::Word
         word_of_the_day = self.new
         word_of_the_day.word_name = "Cat"
         word_of_the_day.part_of_speech = "noun, often attributive"
-        word_of_the_day.definition = "Definition A: a carnivorous mammal (Felis catus) long domesticated as a pet and for catching rats and mice."
+        word_of_the_day.definition = "a carnivorous mammal (Felis catus) long domesticated as a pet and for catching rats and mice."
         word_of_the_day
         return word_of_the_day
+    end
+
+    def self.search_for_word(word)
+        # this needs to return a hash of scrapped info from the word looked up
+        result = self.new
+        result.word_name = word.to_s
+        word
+
     end
 end
