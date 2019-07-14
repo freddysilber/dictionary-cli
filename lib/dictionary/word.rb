cb052339237d4b23061cc0c17eecad885cf51ebd
Freddy Silber <freddy.silber@cloudcoach.com>
2019-07-14 13:29:40 -0600

    attributes to word.rb

--- lib/dictionary/word.rb
+++ lib/dictionary/word.rb
@@ -1,14 +1,14 @@
 class Dictionary::Word
-    # attr_accessor :name
-    # def initialize(name)
-    #     @name = name
-    # end
+    attr_accessor :word_name, :part_of_speech, :definition, :pronounciation
 
-    # def name
-    #     name
-    # end
     def self.today
         puts "HERE IS THE WORD OF THE DAY"
         puts "we are in the word class now"
+        word_1 = self.new
+        word_1.word_name = "Button"
+        word_1.part_of_speech = "Noun"
+        word_1.definition = "A thing you use to push or click."
+        word_1.pronounciation = "{!x Bu + ton}"
+        word_1
     end
 end
