42bfa6df9897fd83c5ee1651d26b287614bd1963
Freddy Silber <freddy.silber@cloudcoach.com>
2019-07-20 08:40:04 -0600

    n

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -1,6 +1,7 @@
-require_relative "./dictionary/version"
+require_relative './dictionary/version'
 require_relative './dictionary/cli'
 require_relative './dictionary/word'
+require_relative './dictionary/scraper'
 require 'nokogiri'
 # require 'pry'
 require 'open-uri'
--- lib/dictionary/scraper.rb
+++ lib/dictionary/scraper.rb
@@ -1,4 +1,6 @@
 class Dictionary:Scraper
-    
-
+    def get_word_of_day
+        doc = Nokogiri::HTML(open("https://www.merriam-webster.com/word-of-the-day"))
+        
+    end
 end
