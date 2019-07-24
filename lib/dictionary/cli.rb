589f3daafcb738e7b9c07fe97ca31f36a4e67de5
Freddy Silber <freddy.silber@cloudcoach.com>
2019-07-23 21:16:39 -0600

    m

--- lib/dictionary/cli.rb
+++ lib/dictionary/cli.rb
@@ -3,95 +3,43 @@
 class Dictionary::CLI
     #####################
     def call
-        welcome_message
+        welcome_message #
         get_todays_word
         show_word_of_the_day
         get_user_input
-        search_for_word
+        find_word
+        show_searched_word
         search_again?
     end
-    #####################
-    def welcome_message
+
+    def welcome_message #
+        puts
         puts "\nWelxome to Merriam Webster online Dictionary!"
         puts "Date: #{Date.today}"
+        puts
         puts "\tThe word of the day is:"
     end
     
-    def get_todays_word # getting the Word of the Day
-        w = Dictionary::Word.todays_word
+    def get_todays_word
+        w = Dictionary::Word.todays_word[0]
         @word_of_the_day = w
-        puts "1 #{@word_of_the_day}"
         if @word_of_the_day == nil || @word_of_the_day == []
-            @word_of_the_day = Dictionary::Scraper.get_word_of_day
-            puts "2 #{@word_of_the_day}"
-            w = Dictionary::Word.new_word_of_the_day(@word_of_the_day)
-            puts w
-            puts "word was nil creating new one"
-            puts @word_of_the_day[:word_name]
-        else
-            puts "FOUND WORD OF THE DAY"
-            puts "#{@word_of_the_day} has stuff"
-            # puts @word_of_the_day.all
+            word = Dictionary::Scraper.get_word_of_day
+            w = Dictionary::Word.new_word_of_the_day(word)
+            @word_of_the_day = w
         end
-        # word = Dictionary::Word.todays_word
-
-        # puts word
-        # if @word_of_the_day == nil
-        #     if word == nil || word == [] || word == ""
-        #         get_word = Dictionary::Scraper.get_word_of_day
-        #         Dictionary::Word.new_word_of_the_day(get_word)
-        #         @word_of_the_day = Dictionary::Word.todays_word
-        #     else
-        #         @word_of_the_day = word
-        #     end
-        # end
-
-        # @word_of_the_day = word
-        # puts "get todays word returned: #{@word_of_the_day}"
-        # puts @word_of_the_day == []
-
-        # w = Dictionary::Scraper.get_word_of_day
-        # Dictionary::Word.new_word_of_the_day(w)
-        # word = Dictionary::Word.todays_word
-        # puts word
-
     end
 
-    def show_word_of_the_day # displaying the Word of the Day
-        # puts "\n--- Word: #{@word_of_the_day[:word_name]}"
-        # puts
-        # wod = Dictionary::Scraper.get_word_of_day
-        # w = Dictionary::Word.new_word_of_the_day(wod)
-        # @word = Dictionary::Word.todays_word
-        # puts "this is the word #{@word}"
-        # puts @word
-        # puts
-
-        # if @word_of_the_day == nil
-        #     wordtoday = Dictionary::Word.todays_word
-        #     puts "Todays word is #{wordtoday}"
-
-        #     @word_of_the_day = Dictionary::Scraper.get_word_of_day
-        #     Dictionary::Word.new_word_of_the_day(@word_of_the_day)
-
-        #     w = Dictionary::Word.todays_word
-        #     puts"word === #{w}"
-
-        #     puts "\n--- #{@word_of_the_day[:word_name]}"
-        #     puts "------ #{@word_of_the_day[:part_of_speech]}"
-        #     puts "------ #{@word_of_the_day[:pronounciation]}"
-        #     puts "------ #{@word_of_the_day[:definition]}"
-        # else
-        #     puts "\n--- #{@word_of_the_day[:word_name]}"
-        #     puts "------ #{@word_of_the_day[:part_of_speech]}"
-        #     puts "------ #{@word_of_the_day[:pronounciation]}"
-        #     puts "------ #{@word_of_the_day[:definition]}"
-        # end
-
+    def show_word_of_the_day
+        puts "\n--- Word: #{@word_of_the_day.word_name.upcase}"
+        puts "------ Part of Speech: #{@word_of_the_day.part_of_speech}"
+        puts "------ Pronounciation: #{@word_of_the_day.pronounciation}"
+        puts "------ Definition: #{@word_of_the_day.definition}"
     end
     
     def get_user_input
-        puts "\n-- Search for a word or type 'exit'..."
+        puts
+        puts "\n-- Search for a word or type 'exit' . . ."
         puts
         input = gets.downcase.strip
         puts
@@ -108,23 +56,33 @@ class Dictionary::CLI
         end
     end
 
-    def search_for_word
-        if @word_to_search != nil
-            puts "\n -- Searching for word: #{@word_to_search.upcase}..."
-            @word_searched = Dictionary::Scraper.search_for_word(@word_to_search)
-            if @word_searched[:word_name] != nil && @word_searched[:part_of_speech] != nil
-                Dictionary::Word.new_word_from_search(@word_searched)
-                # puts @word_searched[:definition].split(":")
-                puts "\n--- #{@word_searched[:word_name]}"
-                puts "------ #{@word_searched[:part_of_speech]}"
-                puts "------ #{@word_searched[:pronounciation]}"
-                puts "------ #{@word_searched[:definition]}"
-            end
+    def find_word
+        search = @word_to_search.downcase
+        @searched = Dictionary::Word.find_a_word(search)
+        if @searched == nil || @searched == []
+            puts "....."
+            word = Dictionary::Scraper.search_for_word(search)
+            w = Dictionary::Word.new_word_from_search(word)
+            @searched = w
+            puts @searched
+            puts "#{@searched.word_name}"
+        else
+            puts "-----"
+            puts @searched
+            puts @searched.word_name
         end
+    end
+
+    def show_searched_word
+        puts "\n--- Word: #{@searched.word_name.upcase}"
+        puts "------ Part of Speech: #{@searched.part_of_speech}"
+        puts "------ Pronounciation: #{@searched.pronounciation}"
+        puts "------ Definition: #{@searched.definition}"
     end
 
     def search_again?
         if @exit != true
+            puts
             puts "\n----------------------------------------------------"
             puts "--- Would you like to search for another word? (Y/N)"
             input = gets.strip
--- lib/dictionary/word.rb
+++ lib/dictionary/word.rb
@@ -5,6 +5,14 @@ class Dictionary::Word
     @@words_of_the_day = []
     @@searched_words = []
 
+    def initialize(word_name = nil, date = nil, part_of_speech = nil, definition= nil, pronounciation = nil)
+        @word_name = word_name
+        @date = date
+        @part_of_speech = part_of_speech
+        pronounciation = pronounciation
+        @definition = definition
+    end
+
     def self.new_word_of_the_day(hash)
         word = self.new
         word.word_name = hash[:word_name]
@@ -14,28 +22,38 @@ class Dictionary::Word
         word.pronounciation = hash[:pronounciation]
         @@all << word
         @@words_of_the_day << word
+        word
     end
 
     def self.new_word_from_search(hash)
-        @@all.each do |w|
-            if w.word_name != hash[:word_name]
-                word = self.new
-                word.word_name = hash[:word_name]
-                word.date = hash[:date]
-                word.part_of_speech = hash[:part_of_speech]
-                word.definition = hash[:definition]
-                word.pronounciation = hash[:pronounciation]
-                @@all << word
-                @@searched_words << word
-            end
-        end
+        word = self.new
+        word.word_name = hash[:word_name]
+        word.date = hash[:date]
+        word.part_of_speech = hash[:part_of_speech]
+        word.definition = hash[:definition]
+        word.pronounciation = hash[:pronounciation]
+        @@all << word
+        @@searched_words << word
+        word
+
+        # @@all.each do |w|
+        #     if w.word_name != hash[:word_name]
+        #         word = self.new
+        #         word.word_name = hash[:word_name]
+        #         word.date = hash[:date]
+        #         word.part_of_speech = hash[:part_of_speech]
+        #         word.definition = hash[:definition]
+        #         word.pronounciation = hash[:pronounciation]
+        #         @@all << word
+        #         @@searched_words << word
+        #         word
+        #     else
+        #         w
+        #     end
+        # end
     end
 
     def self.all
-        @@all
-    end
-
-    def all
         @@all
     end
 
@@ -49,39 +67,28 @@ class Dictionary::Word
 
     def self.todays_word
         @@words_of_the_day.each do |w|
-            # puts w.word_name
-            # puts w.date
             if w.date == Date.today
-                puts "todays_word ---- "
-                puts w.word_name
-                return w
+                w
+            end
+        end
+    end
+
+    def self.find_a_word(search)
+        # @@all.detect {|w_name| w_name == search}
+        @@searched_words.each do |w|
+            puts w.word_name
+            if w.word_name == search || w.word_name === search
+                word = w
+                return word
             else
-                puts "couldnt find word"
                 return nil
             end
         end
-        # word = new_word_of_the_day({:word_name => "WORD OF THE DAY NAME",
-        #                            :date => Date.today,
-        #                            :part_of_speech => "NOUN",
-        #                            :definition => "DEFINITION OF WORD OF THE DAY",
-        #                            :pronounciation => "PRONOUNCIATION"})
-        # word
-
-        # puts "hey"
-        # @@words_of_the_day.each do |w|
-        #     puts w.date
-        #     puts w.word_name
-        #     puts w.date == Date.today
-        #     if w.date != Date.today
-        #         w
-        #     # else
-        #     #     word = Dictionary::Scraper.get_word_of_day
-        #     #     self.new_word_of_the_day(word)
-        #     #     word
-        #     end
-        # end
     end
 end
 # ---------  TO-DO LIST ---------
 # formatting definition ??? (cli)
 #  color coding gem ? or diplay as table. 'tty Table gem'
+
+# >> [1,2,3,4,5,6,7].detect { |x| x.between?(3,4) }
+# => 3
